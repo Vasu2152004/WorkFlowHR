2d8ae7288f33a241ce41b55f54540235c5d9744d
vasu <vkkalavadiya21@gmail.com>
2025-08-09 12:46:11 +0530

    Fix nodemailer createTransport function name

--- api/test-email.js
+++ api/test-email.js
@@ -27,7 +27,7 @@ export default async function handler(req, res) {
     }
 
     // Create transporter
-    const transporter = nodemailer.createTransporter({
+    const transporter = nodemailer.createTransport({
       service: 'gmail',
       auth: {
         user: emailUser,
